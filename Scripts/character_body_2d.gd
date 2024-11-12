a7c012f01475d93b9a369f2f1fb619234a03ffe4
solomonSpence07 <sspence2@ftstudent.org>
2024-11-12 13:16:58 -0500

    start

--- Scripts/character_body_2d.gd
+++ Scripts/character_body_2d.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 100
 
 @onready var sprite : AnimatedSprite2D = $AnimatedSprite2D
 
@@ -14,14 +14,22 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
+	var updown := Input.get_axis("ui_up","ui_down")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if updown:
+		velocity.y = updown * SPEED
+	else:
+		velocity.y = move_toward(velocity.y,0,SPEED)
 
 	set_direction()
 
 	move_and_slide()
+
+	
+
 
 
 func set_direction():
@@ -30,3 +38,15 @@ func set_direction():
 		
 	elif Input.is_action_pressed("ui_right"):
 		sprite.play("right")
+
+
+
+
+	if Input.is_action_pressed("ui_up"):
+		sprite.play("up")
+		
+	elif Input.is_action_pressed("ui_down"):
+		sprite.play("down")
+	
+	else:
+		sprite.play("idle")  # Play "idle" animation when no buttons are pressed

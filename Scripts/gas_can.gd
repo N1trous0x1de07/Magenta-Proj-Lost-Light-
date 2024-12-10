f76f5ea3b7ebcba5d91ec9da3105a7157b3cdf54
solomonSpence07 <sspence2@ftstudent.org>
2024-12-10 13:16:25 -0500

    StartCode

--- Scripts/gas_can.gd
+++ Scripts/gas_can.gd
@@ -1,5 +1,8 @@
 extends Area2D
 
+# This function will be triggered when something enters the part's area
 func _on_body_entered(body):
-	queue_free()
-	print("got it")
+    if body.is_in_group("MainCharacter"):  # Check if it's the player that collided with the part
+        queue_free()  # Remove the part from the scene
+        print("Part collected!")  # Optional: print a message for debugging
+

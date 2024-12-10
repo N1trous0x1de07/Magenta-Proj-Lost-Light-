b7bb0b3d9474074e481ce52d8c3bd23674427ce5
N1tro <robbins_mason@icloud.com>
2024-12-09 20:21:14 -0500

    Auto level change + Added detail
    Created a rough (so far) way to go from level 1 to 2. Added more detail and pathing to level 2 as well as places for the next level switching spots to go.

--- Scripts/GUI/main_menu.gd
+++ Scripts/GUI/main_menu.gd
@@ -10,4 +10,4 @@ func _on_settings_button_pressed():
 
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Maps/WIP Introduction Remake.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Maps/Level_0.tscn")

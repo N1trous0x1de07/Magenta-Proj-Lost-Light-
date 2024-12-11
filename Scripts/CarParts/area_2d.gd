321eb0abc9c2d4feef473e3723a66a55c5fb568a
solomonSpence07 <sspence2@ftstudent.org>
2024-12-11 12:57:31 -0500

    sujhibb

--- Scripts/CarParts/area_2d.gd
+++ Scripts/CarParts/area_2d.gd
@@ -4,5 +4,7 @@ extends Area2D
 func _on_body_entered(body):
     if body.is_in_group("MainCharacter"):  # Check if it's the player that collided with the part
         queue_free()  # Remove the part from the scene
-        print("Part collected!")  # Optional: print a message for debugging
-
+        print("Part collected!")  # Debug message
+        # Notify the GameController
+        var game_controller = get_tree().get_root().get_node("GameController")
+        game_controller.on_part_collected()
